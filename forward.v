385f193eb86ea61808171ee9dc98426bbf1928b0
jamalidosan <79290178+jamalidosan@users.noreply.github.com>
2023-11-12 15:28:30 -0600

    Add files via upload
    updated MEM-to-MEM

--- forward.v
+++ forward.v
@@ -1,4 +1,4 @@
-module forward(input MEM_WB_RegWrite, input EX_MEM_RegWrite, input EX_MEM_MemWrite, input MEM_WB_MemRead, input [4:0] MEM_WB_RegisterRd, input [4:0] EX_MEM_RegisterRd, input [4:0] EX_MEM_RegisterRt, input [4:0] ID_EX_RegisterRs, input [4:0] ID_EX_RegisterRt, output [1:0] ForwardA, output [1:0] ForwardB, output ForwardC);
+module forward(input MEM_WB_RegWrite, input EX_MEM_RegWrite, input [4:0] MEM_WB_RegisterRd, input [4:0] EX_MEM_RegisterRd, input [4:0] EX_MEM_RegisterRt, input [4:0] ID_EX_RegisterRs, input [4:0] ID_EX_RegisterRt, output [1:0] ForwardA, output [1:0] ForwardB, output [1:0] ForwardC);
 	
 	//EX-to-EX
 	assign ForwardA = (MEM_WB_RegWrite & (MEM_WB_RegisterRd != 5'b0) & ~(EX_MEM_RegWrite & (EX_MEM_RegisterRd != 5'b0) & (EX_MEM_RegisterRd != ID_EX_RegisterRs)) & (MEM_WB_RegisterRd == ID_EX_RegisterRs)) ? 2'b01 :
@@ -11,7 +11,7 @@ module forward(input MEM_WB_RegWrite, input EX_MEM_RegWrite, input EX_MEM_MemWri
 		          2'b00;
 
 	//MEM-to-MEM
-	assign ForwardC = ((MEM_WB_RegisterRd == EX_MEM_RegisterRd) & (MEM_WB_RegisterRd != 5'b0)) ? 1 :
-			  0;
+	assign ForwardC = (MEM_WB_RegWrite & (MEM_WB_RegisterRd != 5'b0) & (MEM_WB_RegisterRd == EX_MEM_RegisterRt)) ? 2'b01 :
+			  2'b00;
 
 endmodule
